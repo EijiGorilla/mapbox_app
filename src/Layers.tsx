70c7b6c2966f41a686eeb90c0a1da5ee1863534f
EijiGorilla <emasuz@hotmail.com>
2024-05-06 16:36:54 +0800

    updated

--- src/Layers.tsx
+++ src/Layers.tsx
@@ -84,6 +84,56 @@ const Layers = (props: any) => {
     }
   }
 
+  // Polygon color
+  function getColor(density: any) {
+    return density > 1000
+      ? '#8c2d04'
+      : density > 900
+        ? '#d94801'
+        : density > 800
+          ? '#f16913'
+          : density > 700
+            ? '#fd8d3c'
+            : density > 600
+              ? '#fdae6b'
+              : density > 500
+                ? '#fdd0a2'
+                : density > 400
+                  ? '#fee6ce'
+                  : '#fff5eb';
+  }
+
+  function onStyle(feature: any) {
+    var density = feature.properties.density;
+    return {
+      fillColor: getColor(density),
+      fillOpacity: 0.7,
+      weight: 1,
+      opacity: 0.7,
+      dashArray: 3,
+      color: 'black',
+    };
+    // if (density < 1000) {
+    //   return {
+    //     fillColor: '#FFA500',
+    //     fillOpacity: 0.5,
+    //     weight: 1,
+    //     opacity: 0.7,
+    //     dashArray: 3,
+    //     color: 'black',
+    //   };
+    // } else if (density >= 1000) {
+    //   return {
+    //     fillColor: '#0047AB',
+    //     fillOpacity: 0.5,
+    //     weight: 1,
+    //     opacity: 0.7,
+    //     dashArray: 3,
+    //     color: 'black',
+    //   };
+    // }
+  }
+
   // L.geoJSON(props.data, {
   //   onEachFeature: onEachFeature,
   // }).addTo(map);
@@ -128,6 +178,36 @@ const Layers = (props: any) => {
       {/* <button style={{ width: '100px', height: '20px' }} onClick={() => setUsaData(initialData)}>
         Reset
       </button> */}
+      {/* Reference: https://docs.maptiler.com/leaflet/examples/choropleth-geojson/ */}
+      <div id="state-legend" className="legend">
+        <h4>Population Density of USA </h4>
+        <div>
+          <span style={{ backgroundColor: '#fff5eb' }}></span>
+          {'>'} 1000
+        </div>
+        <div>
+          <span style={{ backgroundColor: '#fee6ce' }}></span>900
+        </div>
+        <div>
+          <span style={{ backgroundColor: '#fdd0a2' }}></span>800
+        </div>
+        <div>
+          <span style={{ backgroundColor: '#fdae6b' }}></span>700
+        </div>
+        <div>
+          <span style={{ backgroundColor: '#fd8d3c' }}></span>600
+        </div>
+        <div>
+          <span style={{ backgroundColor: '#f16913' }}></span>500
+        </div>
+        <div>
+          <span style={{ backgroundColor: '#d94801' }}></span>400
+        </div>
+        <div>
+          <span style={{ backgroundColor: '#8c2d04' }}></span>300
+        </div>
+      </div>
+
       <GeoJSON
         key={JSON.stringify(props.data, usaData, props.state)}
         data={props.data}
@@ -139,7 +219,7 @@ const Layers = (props: any) => {
           },
         }}
         filter={props.state === null ? initialFilterGeo : filterGeo}
-        style={setColor}
+        style={props.data && onStyle}
         onEachFeature={onEachFeature}
       />
       <Marker position={[44.0011, -92.92177]}>
